bc2835b782d1fe331f9fae491034bd2a698a08c7
fgrreloaded <nk.nitish.nk500@gmail.com>
2024-06-28 09:59:33 +0530

    update

--- server.js
+++ server.js
@@ -1,24 +1,3 @@
-// const express = require('express');
-// const path = require('path');
-
-// const app = express();
-
-
-
-// app.use(express.static(path.join(__dirname, 'public')));
-// app.use('/game',express.static(path.join(__dirname, 'game'), {
-//     setHeaders: (res, path) => {
-//         res.set('Cross-Origin-Embedder-Policy', 'require-corp');
-//         res.set('Cross-Origin-Opener-Policy', 'same-origin');
-//     },
-// }));
-
-
-// app.listen(3000, () => {
-//     console.log('Server is running on port 3000');
-// });
-
-
 const express = require('express');
 const path = require('path');
 
@@ -36,7 +15,7 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'game', 'game.html'));
 });
 
-const port = 3000;
+const port = 3000 || process.env.PORT;
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
