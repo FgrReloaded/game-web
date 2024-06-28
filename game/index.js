98788cf5a9ec420bd4c3123ce6cb8941cb8e9733
fgrreloaded <nk.nitish.nk500@gmail.com>
2024-06-28 13:58:46 +0530

    update again

--- game/index.js
+++ game/index.js
@@ -13687,15 +13687,15 @@ const Features = { // eslint-disable-line no-unused-vars
 		if (!Features.isFetchAvailable()) {
 			missing.push('Fetch - Check web browser version');
 		}
-		// if (!Features.isSecureContext()) {
-		// 	missing.push('Secure Context - Check web server configuration (use HTTPS)');
-		// }
-		// if (!Features.isCrossOriginIsolated()) {
-		// 	missing.push('Cross Origin Isolation - Check web server configuration (send correct headers)');
-		// }
-		// if (!Features.isSharedArrayBufferAvailable()) {
-		// 	missing.push('SharedArrayBuffer - Check web server configuration (send correct headers)');
-		// }
+		if (!Features.isSecureContext()) {
+			missing.push('Secure Context - Check web server configuration (use HTTPS)');
+		}
+		if (!Features.isCrossOriginIsolated()) {
+			missing.push('Cross Origin Isolation - Check web server configuration (send correct headers)');
+		}
+		if (!Features.isSharedArrayBufferAvailable()) {
+			missing.push('SharedArrayBuffer - Check web server configuration (send correct headers)');
+		}
 		// Audio is normally optional since we have a dummy fallback.
 		return missing;
 	},
